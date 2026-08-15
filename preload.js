cda777d69cfb89feeb745c10335767c1059681e2
Amit <45531196+amit-tal@users.noreply.github.com>
2026-08-15 20:50:09 +0300

    Bust registration layout cache

--- preload.js
+++ preload.js
@@ -9,7 +9,7 @@ fs.readFileSync = function patchedReadFileSync(path, ...args) {
 
   const isBuffer = Buffer.isBuffer(result);
   let html = isBuffer ? result.toString('utf8') : String(result);
-  const build = '20260815-2043';
+  const build = '20260815-2050';
   const finalLogo = `/assets/amitouch_logo_vector.png?v=${build}`;
 
   html = html
