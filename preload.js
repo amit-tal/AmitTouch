41b2c36c6a1304d06eeaf72804ad38e260609e0f
Amit <45531196+amit-tal@users.noreply.github.com>
2026-08-19 15:50:04 +0300

    Bust cache for contained nav circle

--- preload.js
+++ preload.js
@@ -9,7 +9,7 @@ fs.readFileSync = function patchedReadFileSync(path, ...args) {
 
   const isBuffer = Buffer.isBuffer(result);
   let html = isBuffer ? result.toString('utf8') : String(result);
-  const build = '20260819-1530-splash-nav-liquid-v12';
+  const build = '20260819-1548-nav-contained-circle-v13';
   const finalLogo = `/assets/amitouch_logo_vector.png?v=${build}`;
 
   html = html
