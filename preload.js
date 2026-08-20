23904c77f66c0ca1ef7751da5202cfb5d1d9ee34
Amit <45531196+amit-tal@users.noreply.github.com>
2026-08-20 11:14:50 +0300

    Bump production build for About layout update

--- preload.js
+++ preload.js
@@ -9,7 +9,7 @@ fs.readFileSync = function patchedReadFileSync(path, ...args) {
 
   const isBuffer = Buffer.isBuffer(result);
   let html = isBuffer ? result.toString('utf8') : String(result);
-  const build = '20260820-about-page-v29';
+  const build = '20260820-about-layout-v30';
   const finalLogo = `/assets/amitouch_logo_vector.png?v=${build}`;
 
   html = html
