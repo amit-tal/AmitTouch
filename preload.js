ccda017e34e95c09357a31b5299ae221804eaa07
Amit <45531196+amit-tal@users.noreply.github.com>
2026-08-20 12:42:50 +0300

    Bump build for services flow updates

--- preload.js
+++ preload.js
@@ -9,7 +9,7 @@ fs.readFileSync = function patchedReadFileSync(path, ...args) {
 
   const isBuffer = Buffer.isBuffer(result);
   let html = isBuffer ? result.toString('utf8') : String(result);
-  const build = '20260820-admin-approval-v35';
+  const build = '20260820-services-flow-v36';
   const finalLogo = `/assets/amitouch_logo_vector.png?v=${build}`;
 
   html = html
