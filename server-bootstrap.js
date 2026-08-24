1974d990490eecb8a4d5ad4f6ef912403b88f08e
Amit <45531196+amit-tal@users.noreply.github.com>
2026-08-24 14:01:46 +0300

    Load customer booking availability recovery

--- server-bootstrap.js
+++ server-bootstrap.js
@@ -1,4 +1,5 @@
 import './preload.js';
 import './server-proposal-prepatch.js';
 import './server-reschedule-availability-prepatch.js';
+import './server-booking-availability-prepatch.js';
 await import('./server.js');
