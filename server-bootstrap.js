5c97d38eaf9d0772d24b0777123e9ecd289c0288
Amit <45531196+amit-tal@users.noreply.github.com>
2026-08-25 18:52:31 +0300

    Use one canonical reschedule server patch

--- server-bootstrap.js
+++ server-bootstrap.js
@@ -1,7 +1,5 @@
 import './preload.js';
 import './server-proposal-prepatch.js';
-import './server-customer-response-auth-prepatch.js';
-import './server-proposal-response-prepatch.js';
 import './server-reschedule-availability-prepatch.js';
 import './server-booking-availability-prepatch.js';
 await import('./server.js');
