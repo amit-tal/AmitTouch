e0c368079775667b810160ebc53e6bda1e5b5b6b
Amit <45531196+amit-tal@users.noreply.github.com>
2026-08-25 23:08:07 +0300

    Load admin message endpoint prepatch

--- server-bootstrap.js
+++ server-bootstrap.js
@@ -2,4 +2,5 @@ import './preload.js';
 import './server-proposal-prepatch.js';
 import './server-reschedule-availability-prepatch.js';
 import './server-booking-availability-prepatch.js';
+import './server-admin-message-prepatch.js';
 await import('./server.js');
