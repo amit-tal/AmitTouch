e307b8778005d01ee38b900a913913b8188fd449
Amit <45531196+amit-tal@users.noreply.github.com>
2026-08-27 12:11:27 +0300

    Delete legacy admin settings page permanently

--- admin-settings-page.js
+++
@@ -1,6 +0,0 @@
-(function(){'use strict';
-/* Legacy settings implementation intentionally removed. The canonical settings screen is loaded by admin-settings-route-fix.js. */
-if(window.__AMIT_ADMIN_SETTINGS_V4__)delete window.__AMIT_ADMIN_SETTINGS_V4__;
-document.querySelectorAll('style[id^="amit-admin-settings-v"]').forEach(x=>x.remove());
-document.querySelectorAll('.ast,.ast-sub').forEach(x=>x.remove());
-})();
